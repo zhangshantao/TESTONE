c655a94f9e8189ee1ac6d55dc3b6130b18937078
zhangshantao <1879957276@qq.com>
2020-01-02 15:03:09 +0800

    hello

---
+++ testoo1/src/one/hello.java
@@ -0,0 +1,23 @@
+package one;
+
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class hello {
+    public  void main(String[] args){
+      List
+
+    }
+
+    public static void main(String[] args) {
+       /* Callable<Object> callable = new Callable<Object>() {
+            public Object call() throws Exception {
+               int i=0;*/
+
+       System.out.println();
+            }
+        };
+    public static FINAL
+
+}
