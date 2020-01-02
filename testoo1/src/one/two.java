1b1faf8cfe2f9ba263cd22ec262db0915448a9d1
zhangshantao <1879957276@qq.com>
2020-01-02 15:15:33 +0800

    TWO

---
+++ testoo1/src/one/two.java
@@ -0,0 +1,24 @@
+package one;
+
+import java.util.Iterator;
+import java.util.concurrent.Callable;
+
+/**
+ * @author 张善涛
+ * @create 2020-01-01 22:06
+ */
+public class two {
+    public static void main(String[] args) {
+        Callable<Object> callable = new Callable<Object>() {
+            public Object call() throws Exception {
+
+                
+            }
+        };
+            
+        }
+    }
+}
+    private static final String KEY_ = "";
+// FIXME: 2020/1/1 <code>false</code>
+private static final String TAG = "two";
